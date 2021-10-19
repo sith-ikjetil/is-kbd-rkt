c005f5bc678a3057cf3a576f8ae98e8d5ce87b6f
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2021-10-19 14:42:15 +0200

    Got RCBA

--- src/lkm/is-kbd-rkt.c
+++ src/lkm/is-kbd-rkt.c
@@ -14,6 +14,8 @@
 #include <linux/module.h>
 #include <linux/device.h>
 #include <linux/fs.h>
+#include <asm-generic/iomap.h>
+#include <linux/pci.h>
 
 //
 // MODULE metadata
@@ -24,19 +26,21 @@ MODULE_LICENSE("GPL");
 MODULE_VERSION("1.2");
 
 //
+// #define
+//
+#define INTEL_LPC_RCBA_REG 0xF0
+
+//
 // function prototypes
 //
-//static int device_open(struct inode *, struct file *);
-//static int device_release(struct inode *, struct file *);
 static ssize_t device_read(struct file *, char *, size_t, loff_t *);
-//static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
-static int GatherData(IS_KEYBOARD_RKT_DATA* p);
+static u32 GetRootComplexBaseAddress(void);
+static void GatherData(IS_KEYBOARD_RKT_DATA* p);
 
 //
 // static variables
 //
 static int major_num;
-//static int device_open_count = 0;
 static struct class* char_class = NULL;
 static struct device* char_device = NULL;
 
@@ -66,50 +70,65 @@ static ssize_t device_read(struct file *flip, char *buffer, size_t len, loff_t *
 }
 
 //
-// device write
+// GetRootComplexBaseAddress
 //
-//static ssize_t device_write(struct file *file, const char *buffer, size_t len, loff_t *offset) 
-//{
-//   return 0;
-//}
-
-//
-// device open
-//
-//static int device_open(struct inode *inode, struct file *file) 
-//{
-//    if ( device_open_count ) {
-//        return -EBUSY;
-//    }
-
-//    device_open_count++;
-//	try_module_get(THIS_MODULE);
-    
-//	return 0;
-//}
-
-//
-// device_release
-//
-//static int device_release(struct inode *inode, struct file *file) 
-//{
-//   device_open_count--;
-//    module_put(THIS_MODULE);
-//    
-//	return 0;
-//}
+static u32 GetRootComplexBaseAddress(void)
+{
+	struct pci_dev *dev = NULL;
+	u32 rcba;
+	
+	rcba = 0;	
+	dev = pci_get_device(PCI_VENDOR_ID_INTEL, PCI_ANY_ID, NULL);
+_retry:
+	if ( dev != NULL ) {
+		pci_bus_read_config_dword(dev->bus, PCI_DEVFN(31, 0), INTEL_LPC_RCBA_REG, &rcba);
+		if ( rcba == 0 ) {
+			dev = pci_get_device(PCI_VENDOR_ID_INTEL, PCI_ANY_ID, dev);
+			goto _retry;
+		}
+	}
+	
+	return rcba;
+}
 
 //
 // GatherData
 //
-static int GatherData(IS_KEYBOARD_RKT_DATA* p)
+static void GatherData(IS_KEYBOARD_RKT_DATA* p)
 {
 	memset((void*)p, 0, sizeof(IS_KEYBOARD_RKT_DATA));
-
 	p->cbSize = sizeof(IS_KEYBOARD_RKT_DATA);
-	strcpy(p->szErrorMessage, "Hello World from Kjetil Kristoffer Solberg");
 
-	return 0;
+	//unsigned short PCI_CONFIGURATION_SPACE_ADDRESS_PORT	= 0x0cf8;// Configuration Space Address 
+	//unsigned short PCI_CONFIGURATION_SPACE_DATA_PORT	= 0x0cfc;// Configuration Space Data 
+
+	//
+	// RCBA
+	//
+	p->dwRootComplexBaseAddress = GetRootComplexBaseAddress();
+	if ( p->dwRootComplexBaseAddress == 0 ) {
+		strcpy(p->szErrorMessage, "Invalid Root Complex Base Address");
+	}
+
+	//
+	// IRQTn
+	//
+	void* pmm = p->dwRootComplexBaseAddress + 0x1E80;
+
+	/*volatile u32* pIOTR0 = (u32*)((u64)p->dwRootComplexBaseAddress + 0x1E80);
+    volatile u32* pIOTR1 = (u32*)((u64)p->dwRootComplexBaseAddress + 0x1E88);
+    volatile u32* pIOTR2 = (u32*)((u64)p->dwRootComplexBaseAddress + 0x1E90);
+    volatile u32* pIOTR3 = (u32*)((u64)p->dwRootComplexBaseAddress + 0x1E98);
+*/
+    p->qwIOTRn[0] = *((u64*)pmm);//readl(pIOTR0);
+    /*p->qwIOTRn[0] |= ((u64)(*(pIOTR0 + 1) << 32));
+    p->qwIOTRn[1] = *pIOTR1;
+    p->qwIOTRn[1] |= ((u64)(*(pIOTR1 + 1) << 32));
+    p->qwIOTRn[2] = *pIOTR2;
+    p->qwIOTRn[2] |= ((u64)(*(pIOTR2 + 1) << 32));
+    p->qwIOTRn[3] = *pIOTR3;
+    p->qwIOTRn[3] |= ((u64)(*(pIOTR3 + 1) << 32));
+*/
 }
 
 //
@@ -118,9 +137,6 @@ static int GatherData(IS_KEYBOARD_RKT_DATA* p)
 static struct file_operations file_ops = {
 	.owner = THIS_MODULE,
     .read = device_read,
-//    .write = device_write,
-//    .open = device_open,
-//    .release = device_release,
 };
 
 //
