47533b43aa7db7f2acbeba8e99571f71602df310
Kjetil Kristoffer Solberg <kjetil.kristoffer.solberg@outlook.com>
2022-07-21 15:57:41 +0200

    Improved lkm scripts

--- src/lkm/cp_ko_to_extra.sh
+++ src/lkm/cp_ko_to_extra.sh
@@ -1,4 +1,16 @@
-DIR="/lib/modules/`uname -r`/extra"
-[ ! -d "$DIR" ] && mkdir $DIR
+#!/bin/bash
+if [[ $# -eq 1 ]] 
+then
+    DIR="/lib/modules/`uname -r`/extra"
+    [ ! -d "$DIR" ] && mkdir $DIR
 
-cp $1 $DIR
+    cp $1 $DIR
+else
+    echo "##"
+    echo "## Copy Kernel Object to /lib/modules/<uname -r>/extra"
+    echo "##"
+    echo "## Usage: cp_ko_to_extra.sh <./filename.ko>"
+    echo "##"
+    echo "## Error #############################################"
+    echo "Wrong number of arguments."
+fi
--- src/lkm/sign_ko.sh
+++ src/lkm/sign_ko.sh
@@ -1,2 +1,14 @@
-echo "Signing $1"
-/usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /root/module-signing/new.MOK.priv /root/module-signing/MOK.der "$1"
+#!/bin/bash
+if [[ $# -eq 1 ]] 
+then
+    echo "Signing $1"
+    /usr/src/linux-headers-$(uname -r)/scripts/sign-file sha256 /root/module-signing/new.MOK.priv /root/module-signing/MOK.der "$1"
+else
+    echo "##"
+    echo "## Sign Kernel Object"
+    echo "##"
+    echo "## Usage: sign_ko.sh <./filename.ko>"
+    echo "##"
+    echo "## Error #############################################"
+    echo "Wrong number of arguments."
+fi
